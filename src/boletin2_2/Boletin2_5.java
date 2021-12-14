65034172d803839e5307c8f481cde5a15b17bfcc
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-12-14 10:42:30 +0100

    Programación

---
+++ src/boletin2_2/Boletin2_5.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_2;
+import java.util.Scanner;
+/**
+ *
+ * @author dam1
+ */
+public class Boletin2_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    /*float lado=3f;
+    /*
+    float area=lado*lado;
+    System.out.println("o area de cadrado é="+area);*/
+   
+    /*float area=(float)Math.pow(3, 2);   
+    System.out.println("o area do cadrado é ="+area);*/
+   
+    float euros;
+    /*String frase;*/
+    
+    /*Scanner sc=new Scanner (System.in);
+    System.out.println(" teclea euros: ");
+    
+    euros=sc.nextFloat();
+    
+    System.out.println(" euros =" +euros);    
+    
+    float euroADolar;
+    System.out.println(" teclea euroADolar: ");
+    euroADolar=sc.nextFloat();
+    System.out.println(" euroADolar="+euroADolar);
+    
+    float dolares=euros*euroADolar;
+    System.out.println(euros+ "euros equivalen a "+ dolares+ "dolares" );*/
+   
+    /*int num1;
+    String frase;
+    
+    Scanner sc=new Scanner (System.in);
+    System.out.println(" teclea o num1: ");
+    
+    num1=sc.nextInt();
+    
+    System.out.println("num1="+num1);
+    
+    int num2;
+    System.out.println(" teclea o num2: ");
+   
+    num2=sc.nextInt();
+    
+    System.out.println("num2="+num2);
+    
+    int suma=num1+num2;
+    int resta=num1-num2;
+    int resta2=num2-num1;
+    int producto=num1*num2;
+    int cociente=num1/num2;
+    int cociente2=num2/num1;
+    
+    System.out.println("a suma="+suma);
+    System.out.println(" a resta="+resta);
+    System.out.println(" a resta2="+resta2);
+    System.out.println(" o producto="+producto);
+    System.out.println(" o cociente="+cociente);
+    System.out.println(" o cociente2="+cociente2);*/
+    
+    
+    double millas, metros;   
+    
+    final double EQUIVALENCIA =1852;
+    
+    Scanner sc=new Scanner (System.in);
+   
+    System.out.println("Introduce la distancia en millas mariñas: ");
+    millas=sc.nextFloat();
+    //metros=millas*EQUIVALENCIA;
+    //System.out.println(millas+" millas mariñas equivalen a: "+metros+" metros ");
+    System.out.println(millas+" millas mariñas equivalen a: "+(millas*EQUIVALENCIA)+" metros ");
+    
+    
+    
+   
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
